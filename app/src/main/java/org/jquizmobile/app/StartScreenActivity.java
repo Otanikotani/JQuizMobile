70f6ed93217fa5e2b866fcd7435965ffa3d4c632
Tofik Mamishov <mamishov.tofik@gmail.com>
2015-06-28 14:27:57 +0300

    - StartScreenActivity - QuestionActivity

---
+++ app/src/main/java/org/jquizmobile/app/StartScreenActivity.java
@@ -0,0 +1,25 @@
+package org.jquizmobile.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+
+    public void onStartClicked(View v) {
+        Intent questionActivity = new Intent(this, QuestionActivity.class);
+        startActivity(questionActivity);
+    }
+
+    public void onHighscoreClicked(View v) {
+
+    }
+}
