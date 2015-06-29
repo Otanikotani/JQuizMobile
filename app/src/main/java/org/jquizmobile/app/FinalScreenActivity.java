9dd00d611445827a31c851d3b9e2eca47d1910b3
Tofik Mamishov <mamishov.tofik@gmail.com>
2015-06-29 16:05:56 +0300

    - bla-bla

---
+++ app/src/main/java/org/jquizmobile/app/FinalScreenActivity.java
@@ -0,0 +1,22 @@
+package org.jquizmobile.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class FinalScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_screen);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent startScreenActivity = new Intent(this, StartScreenActivity.class);
+        startActivity(startScreenActivity);
+    }
+}
